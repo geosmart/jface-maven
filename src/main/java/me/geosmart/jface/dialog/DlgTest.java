109ab5bf52c73596727b0056ff0c4e01c2fce0f5
geosmart <geosmart@foxmail.com>
2014-11-22 11:08:49 +0800

    test

--- src/main/java/me/geosmart/jface/dialog/DlgTest.java
+++ src/main/java/me/geosmart/jface/dialog/DlgTest.java
@@ -9,6 +9,11 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
+/**
+ * 测试窗口
+ * 
+ * @author geosmart
+ */
 public class DlgTest extends Dialog
 {
 	
